f57d808dd171cfccb40f3d30a6557036815f38e6
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-02-22 16:18:29 +0700

    add docker

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:3.9-bullseye
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+ENV VIRTUAL_ENV=/opt/venv
+
+RUN python3 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+RUN apt update -y
+
+RUN mkdir /app
+
+RUN ls /app
+WORKDIR /app
+
+
+ADD . /app/
+
+RUN pip install --upgrade pip
+RUN pip install poetry
+RUN poetry install
+
+# ADD . /app/
--- src/core/settings/development.py
+++ src/core/settings/development.py
@@ -2,6 +2,30 @@ from .base import *
 
 DEBUG = True
 
+"""
+production settings
+"""
+import os
+from dotenv import load_dotenv
+from .base import *
+
+load_dotenv(os.path.join(BASE_DIR,".env"))
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = os.getenv("DEBUG")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+
+}
 INSTALLED_APPS += [
     
 ]
