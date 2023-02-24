ad68cec873711fa6fc14870d99c2ebfb7b5cfcd7
perymerdeka <tkjasikk@gmail.com>
2023-02-24 16:27:58 +0700

    JSON Render App

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -19,6 +19,6 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
