e440967e43963c2e73852afd87fac0fd3c3c7940
Muchamad Faiz <109614025+muchamadfaiz@users.noreply.github.com>
2023-03-13 16:51:05 +0700

    Done:implement jwt drf :D

--- src/core/settings/development.py
+++ src/core/settings/development.py
@@ -16,16 +16,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PWD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
 }
+
 INSTALLED_APPS += [
     
 ]
--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -18,7 +18,11 @@ DATABASES = {
 # REST FRAMEWORK
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.JSONRenderer', 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         # 'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
